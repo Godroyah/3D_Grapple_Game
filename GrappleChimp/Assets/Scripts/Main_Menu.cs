725c4e32d9990e19325666aac141472d0f014c9e
Brendan Winston <bwinsto1@fsc.int>
2019-05-14 19:26:10 -0400

    finish work on main menu

---
+++ GrappleChimp/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+using UnityEngine;
+
+public class Main_Menu : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject playMenu;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene("testinggreybox");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void HowToPlay()
+    {
+        mainMenu.SetActive(false);
+        playMenu.SetActive(true);
+    }
+
+    public void Back()
+    {
+        playMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+}
